172d5ef077829d9cd55956f450f20aae49335a88
Benjamin <benjamin.poulain@coopmcs.com>
2021-10-11 17:04:32 +0200

    Ajout de l'interface graphique

---
+++ gui.pl
@@ -0,0 +1,111 @@
+############################################ Load assets (icons and bitmaps) ############################################
+foreach (qw/folder_16x16.ico file_16x16.ico/)  {
+    my $path = '';
+    if (-e "gfx/$_") {
+        $path = "gfx/$_";
+    } elsif (-e "$ENV{PAR_TEMP}/inc/gfx/$_") {
+        $path = "$ENV{PAR_TEMP}/inc/gfx/$_";
+    }
+    # load ico or bmp
+    if (/\.ico$/) {
+        $assets{$_} = Win32::GUI::Icon->new( $path );
+    } elsif (/\.bmp$/) {
+        $assets{$_} = Win32::GUI::Bitmap->new( $path );
+    }
+}
+
+
+$main = Win32::GUI::Window->new(
+    -name => 'Main',
+    -text => 'mbox2pst',
+    -width => 520,
+    -height=> 230,
+    # -menu   => $menu,
+    -left => CW_USEDEFAULT
+);
+$main->Center();
+
+# status bar pour les messages
+$main->AddStatusBar(
+    -name => 'StatusBar',
+    -text => '',
+    -width => 600,
+    -height => 20
+);
+
+# choix du répertoire d'input
+# libelle pour nom du fichier output
+$main->AddLabel(
+    -pos => [ 20, $y ],
+    -text => "Répertoire mbox"
+);
+
+# texte qui contient le fichier de sortie
+$main->AddTextfield(
+    -name 	=> 'TextfieldInputDir',
+    -pos 	=> [100, $y],
+    -size	=> [300, 20],
+    -text 	=> $ENV{'HOME'}.'\\.Mail'
+);
+
+# bouton pour choisir le fichier de sortie
+$main->AddButton(
+    -name 	=> 'ButtonChooseInputDir',
+    -pos    => [ 410, $y-5 ],
+    -size   => [28,28],
+    -icon   => $assets{'folder_16x16.ico'}
+);
+
+$y+=30;
+
+
+# libelle pour nom du fichier output
+$main->AddLabel(
+    -pos => [ 20, $y ],
+    -text => 'Fichier PST'
+);
+
+# texte qui contient le fichier de sortie
+$main->AddTextfield(
+    -name 	=> 'TextfieldOutputFilename',
+    -pos 	=> [100, $y],
+    -size	=> [300, 20],
+    -text 	=> $ENV{'HOME'}.'\\Desktop\\Outlook.pst'
+);
+
+# bouton pour choisir le fichier de sortie
+$main->AddButton(
+    -name 	=> 'ButtonChooseOutputFilename',
+    -pos    => [ 410, $y-5 ],
+    -size   => [28,28],
+    -icon   => $assets{'file_16x16.ico'}
+);
+
+$y+=30;
+
+# executer le programme externe
+$main->AddButton(
+	-name => 'ButtonConvert',
+	-pos => [ 20, $y ],
+	-text => "Convertir",
+    -size => [460, 50],
+);
+
+$y+=60;
+
+# executer le programme externe
+$main->AddProgressBar(
+	-name => 'ProgressBar',
+	-pos => [ 20, $y ],
+    -background=>[0,255,85],
+    -smooth   => 1,
+    -size => [460, 20]
+);
+
+
+# affiche la fenetre principale
+$main->Show();
+Win32::GUI::DoEvents();
+Win32::GUI::Dialog();
+
+exit(0);
--- mbox2pst.pl
+++ mbox2pst.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 use Email::PST::Win32;
+use Data::Dumper;
 use Data::Uniqid qw(luniqid); 	# copy message into a temp file
 use File::Find;
 use File::Path;
@@ -7,17 +8,93 @@ use strict;
 use File::Basename;
 use Cwd;
 use Getopt::Long;				# manage options
+
+# for gui
+#use Win32::API;
+use Win32::GUI();
+use Win32::GUI::Constants qw(CW_USEDEFAULT WS_OVERLAPPEDWINDOW WS_DISABLED WS_POPUP WS_CAPTION WS_THICKFRAME WS_EX_TOPMOST SW_HIDE);
+use Win32::GUI::BitmapInline;
+
 $|=1;
 
-my ($mboxdir,$pst,$tempdir,$help,$quiet);
-GetOptions(	'mboxdir=s'=>\$mboxdir, 'pst=s'=>\$pst, 'tempdir=s'=>\$tempdir, 'help|usage!'=>\$help, 'quiet!'=>\$quiet) ;
+my ($mboxdir,$pst,$tempdir,$gui,$help,$quiet);
+GetOptions(	'mboxdir=s'=>\$mboxdir, 'pst=s'=>\$pst, 'tempdir=s'=>\$tempdir, 'gui!'=>\$gui,'help|usage!'=>\$help, 'quiet!'=>\$quiet) ;
 
-my $program_dir = getcwd;
-print "Working directory : $program_dir\n" unless $quiet;
+our ($main,%assets,$program_dir,$outlook_box,$pstObj,$total_message,$current_message,$mbox) ;
+our $y=10;
 
-die <<EOT if ($help);
+# cree la ffenetre principale
+if ($gui) {
+    my $DOS = Win32::GUI::GetPerlWindow();    
+    Win32::GUI::Hide($DOS);
+    require('./gui.pl');
+} else {
+    do_convert();
+}
+
+
+sub do_convert {
+    $main->StatusBar->Text("Convertion en cours") if $gui;
+
+    $program_dir = getcwd;
+    print "Working directory : $program_dir\n" unless $quiet;
+
+    display_help() if $help;
+
+    die "Option --mboxdir=<mbox_input_directory> is needed" unless length($mboxdir)>0;
+    die "$mboxdir is not a directory" if !-d $mboxdir;
+    $mboxdir =~ s|\\|\/|g; # convert \ to /
+    $mboxdir =~ s|/$||; # remove last /
+
+    print "Input directory : $mboxdir\n" unless $quiet;
+
+    $tempdir = $tempdir || "$program_dir/tmp";
+    mkpath($tempdir);
+
+    $pst = $pst || "$program_dir/out/Outlook.pst";
+    mkpath(dirname($pst));
+    print "Export PST file : $pst\n" unless $quiet;
+
+    # clean up last run
+    print "Clean up last run... " unless $quiet;
+    unlink($pst);
+    find(\&clean_last_run, $tempdir);
+    print "OK" unless $quiet;
+
+    # creat new PST file
+    $pstObj = Email::PST::Win32->new(
+        filename     => $pst,
+        display_name => 'Export from mbox2pst',
+    );
+
+    # Errors may occur when high numbers of items are added.
+    # A count_per_session > 0 will determine when to close and
+    # reopen the PST file. The default value is 1000.
+    $pstObj->count_per_session( 2000 );
+    
+    # Get number of MIME files added
+    my $count = $pstObj->instance_counter;
+
+    $total_message = 0; # total number of message in current mbox file
+    $outlook_box = ''; # name of outlook box (inbox, trash, ...)
+    $current_message = 0; # index of current message
+
+    find(\&extractMboxToFiles, $mboxdir);
+
+    # fini
+    $main->StatusBar->Text("$mbox : Convertion termin\x{e9}e") if $gui;
+
+    # Close the PST file
+    $pstObj->close;
+} # end of do_convert
+
+sub display_help {
+    die <<EOT ;
 Options :
---mboxdir=<mbox_input_directory> (required)
+--gui
+    Launch user interface
+
+--mboxdir=<mbox_input_directory> (required if no GUI)
 	The mbox directory you want to convert
 
 --pst=<output_pst_file>
@@ -32,59 +109,24 @@ Options :
 --quiet
 	Don't print anything
 EOT
+}
 
-die "Option --mboxdir=<mbox_input_directory> is needed" unless length($mboxdir)>0;
-die "$mboxdir is not a directory" if !-d $mboxdir;
-$mboxdir =~ s|\\|\/|g; # convert \ to /
-$mboxdir =~ s|/$||; # remove last /
-
-print "Input directory : $mboxdir\n" unless $quiet;
-
-$tempdir = $tempdir || "$program_dir/tmp";
-mkpath($tempdir);
-
-$pst = $pst || "$program_dir/out/Outlook.pst";
-mkpath(dirname($pst));
-print "Export PST file : $pst\n" unless $quiet;
-
-# clean up last run
-print "Clean up last run... " unless $quiet;
-unlink($pst);
-find(\&clean_last_run, $tempdir);
 sub clean_last_run {
 	return if ($_ eq '.' || $_ eq '..');
     unlink($_) if (/\.mail$/i);
 }
-print "OK" unless $quiet;
 
-# creat new PST file
-my $pst = Email::PST::Win32->new(
-    filename     => $pst,
-    display_name => 'Export from mbox2pst',
-);
 
-# Errors may occur when high numbers of items are added.
-# A count_per_session > 0 will determine when to close and
-# reopen the PST file. The default value is 1000.
-$pst->count_per_session( 2000 );
- 
-# Get number of MIME files added
-my $count = $pst->instance_counter;
-
-my $buffer = ''; # buffer for current message
-my $total_message = 0;
-
-my $mbox = '';
-my $outlook_box = '';
-
-find(\&extractMboxToFiles, $mboxdir);
 sub extractMboxToFiles {
+    my $buffer = '';
     $mbox = $File::Find::name;
     $mbox =~ s/^$mboxdir\/?//; # remove leading path
 
 	return if ($_ eq '.' || $_ eq '..' || -d $_ || /\.msf$/ || $_ eq 'filterlog.html' || $_ eq 'msgFilterRules.dat');
 
-    my $total_message = 0;
+    $main->StatusBar->Text("$mbox : Extraction des mails") if $gui;
+
+    $total_message = 0;
 
     print "\nExtracting message from '$mbox' " unless $quiet;
 
@@ -115,17 +157,62 @@ sub extractMboxToFiles {
     $outlook_box =~ s/Draft/Brouillons/;
     print "\nAdd message from '$mbox' to '$outlook_box'\n" unless $quiet;
 
+    $current_message = 1;
+    $main->ProgressBar->SetRange(0,$total_message) if $gui;
     find(\&add_to_pst, $tempdir);
 }
+
 
 # add *.mail to pst file
 sub add_to_pst {
 	return if ($_ eq '.' || $_ eq '..');
     if (/\.mail$/i) {
-        $pst->add_mime_file( $_, $outlook_box, 1 ? 'note' : 'post' );
+        $pstObj->add_mime_file( $_, $outlook_box, 1 ? 'note' : 'post' );
         unlink($_);
+        $current_message++;
+        updateProgressBar() if $gui;
     }
 }
 
-# Close the PST file
-$pst->close;
+
+############################################################# EVENT GUI ##########################################
+
+# selectionne le répertoire a convertir
+sub ButtonChooseInputDir_Click {
+    my $dir = Win32::GUI::BrowseForFolder (
+        -title     => "Choissiez un répertoire mbox",
+        -directory => $ENV{'HOME'}.'\\.Mail',
+        -folderonly => 1,
+    );
+    $main->TextfieldInputDir->Text($dir);
+}
+
+
+# selectionne le fichier de sortie CSV
+sub ButtonChooseOutputFilename_Click {
+	my @file = Win32::GUI::GetOpenFileName(
+		-filter => ['PST - Outlook format', '*.pst',
+					'All files - *.*', '*'
+					],
+		-directory => $ENV{'HOME'}.'\\Desktop',
+		-title => 'Choissiez un fichier de sortie PST',
+		-file => 'Outlook.pst'
+	);
+	$main->TextfieldOutputFilename->Text($file[0]);
+}
+
+# selectionne le fichier de sortie CSV
+sub ButtonConvert_Click {
+    $mboxdir = $main->TextfieldInputDir->Text();
+    $pst     = $main->TextfieldOutputFilename->Text();
+	do_convert();   
+}
+
+sub updateProgressBar {
+    #my $pourcentage = ($current_message / $total_message) * 100;
+    Win32::GUI::DoEvents() >= 0;
+    $main->ProgressBar->SetPos($current_message);
+    $main->StatusBar->Text("$mbox : message $current_message / $total_message ".sprintf('(%0.1f %%)',($current_message / $total_message) * 100));
+    #$main->ProgressBar->StepIt();
+    #print "update progressbar to \$current_message=$current_message / \$total_message=$total_message) pourcentage=$pourcentage\n";
+}
